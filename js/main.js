7d05f95a0690a2435530b31d8b3ce0fdaf9854b0
GolosovRuslan <77554486+GolosovRuslan@users.noreply.github.com>
2025-07-01 16:58:02 +0300

    Доработан функционал ДЗ 4.9

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 const getRandomPositiveInteger = (min, max = 1) => {
     if (min === undefined) {
-        throw new Error("Первый параметр должен быть число")
+        throw new Error("Первый параметр должен быть число");
     }
 
     min = Math.ceil(Math.abs(min));
     max = Math.floor(Math.abs(max));
-    const number = Math.floor(Math.random() * (max - min)) + min;
+    const number = Math.floor(Math.random() * (max - min + 1)) + min;
     return number;
 };
 
-// const checkStringLength = (string, length) => {
-//     return string.length <= length;
-// };
+const usedIds = new Set();
+const getUniqueRandomId = (PICTURE_COUNT) => {
+    let id;
+    do {
+        id = getRandomPositiveInteger(1, PICTURE_COUNT);
+    } while (usedIds.has(id));
+
+    usedIds.add(id);
+    return id;
+};
 
 const getRandomArrayElements = (elements) => {
     return elements[getRandomPositiveInteger(0, elements.length - 1)];
@@ -19,11 +26,11 @@ const getRandomArrayElements = (elements) => {
 
 const COMMENT_LINES = [
     'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
 const NAMES = [
@@ -36,9 +43,10 @@ const NAMES = [
 const PICTURE_COUNT = 25;
 
 const createPicture = () => {
+    const uniqueId = getUniqueRandomId(PICTURE_COUNT);
     return {
-        id: getRandomPositiveInteger(1, 25),
-        avatar: `photos/${getRandomPositiveInteger(1, 25)}.jpg`,
+        id: uniqueId,
+        avatar: "photos/" + uniqueId + ".jpg",
         message: getRandomArrayElements(COMMENT_LINES),
         name: getRandomArrayElements(NAMES),
         likes: getRandomPositiveInteger(15, 250)
@@ -46,5 +54,4 @@ const createPicture = () => {
 };
 
 const generatePhoto = Array.from({ length: PICTURE_COUNT }, createPicture);
-
 console.log(generatePhoto);
